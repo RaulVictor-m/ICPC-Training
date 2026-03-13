75dfd4820d0f618383df3c4f87cb79be36602a24
RaulVictor-m <raulvictor822@gmail.com>
2026-03-13 15:36:09 -0300

    Array Description CheckPoint

--- dp/ArrayDescription/main.cpp
+++ dp/ArrayDescription/main.cpp
@@ -22,19 +22,40 @@ typedef long long ll;
 
 const ll MAXN = 1e5+1;
 const ll MAXM = 101;
+const ll MOD = 1e9+7;
 
 ll m_array[MAXN] = {0};
 ll dp[MAXN][MAXM] = {0};
 
 ll dpI(ll n, ll m) {
+    if (n == 1) return m_array[n] == 0 ? m : 1;
 
+    if (m_array[1] == 0) for (int j = 1; j <= m; j++) dp[1][j] = 1;
+    else dp[1][m_array[1]] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (m_array[i] == 0 || j == m_array[i]) {
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j] + dp[i-1][j+1]) % MOD;
+            }
+        }
+    }
+
+    if (m_array[n] != 0) return dp[n][m_array[n]];
+
+    ll result = 0;
+    for (int j = 1; j <= m; j++) result = (dp[n][j] + result) % MOD;
+
+    return result;
 }
 
 int main(void) {
     int n, m; cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >>  m_array[i];
     }
+
+    cout << dpI(n, m) << endl;
     return 0;
 }
