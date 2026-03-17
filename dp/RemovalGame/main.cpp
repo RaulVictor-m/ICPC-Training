0425f210399ec18b6ea694948f3cdcf8654cd34a
RaulVictor-m <raulvictor822@gmail.com>
2026-03-17 01:41:57 -0300

    Removal Game Explanation

--- dp/RemovalGame/main.cpp
+++ dp/RemovalGame/main.cpp
@@ -10,6 +10,16 @@ typedef long long ll;
 
 /*
     REASON:
+        right = (sum(start+1:end) - dp(start+1, end)) + coin[start]
+        left  = (sum(start:end-1) - dp(start, end-1)) + coin[start]
+
+
+        dp(start, end) = max(right, left);
+
+        right is basically the total of the area start+1 -- end minus 
+        the dp that area which gives the maximum value possible for a player
+        that plays in that condition
+        left is basically the same but to the other side
 
 */
 const ll MAXN = 5001;
@@ -26,8 +36,6 @@ ll dpRec(int start, int end) {
     ll right = ((prefix[end] - prefix[start]) - dpRec(start + 1, end)) + vals[start];
     ll left  = ((prefix[end-1] - prefix[start-1]) - dpRec(start, end - 1)) + vals[end];
 
-    // cout << (prefix[end-1] - prefix[start-1]) << endl;;
-
     return dp[start][end] = max(right, left);
 }
 
@@ -39,20 +47,13 @@ int main(void) {
             dp[i][j] = INT_MIN;
         }
     }
+
     for (int i = 1; i <= n; i++) {
         cin >> vals[i];
         prefix[i] = prefix[i-1] + vals[i];
     }
 
     cout << dpRec(1, n) << endl;
-    // for (int i = 1; i <= n; i++) {
-    //     for (int j = 1; j <= n; j++) {
-    //         if (dp[i][j] != INT_MIN) 
-    //         cout << dp[i][j] << "  ";
-    //         else cout << "    ";
-    //     }
-    //     cout << endl;
-    // }
 
     return 0;
 }
