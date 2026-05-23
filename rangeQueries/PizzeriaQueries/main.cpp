fbe7b7757509808dbb827f0edde96df915c640c7
RaulVictor-m <raulvictor822@gmail.com>
2026-05-23 16:47:10 -0300

    Pizzeria Queries - DONE

--- rangeQueries/PizzeriaQueries/main.cpp
+++ rangeQueries/PizzeriaQueries/main.cpp
@@ -1,7 +1,7 @@
 /*
  * LINK: https://cses.fi/problemset/task/2206
  * NAME: PizzeriaQueries
- * STATE: BROKEN - no idea
+ * STATE: DONE
 */
 
 #include <bits/stdc++.h>
@@ -11,15 +11,20 @@ typedef long long ll;
 
 /*
     REASON:
-
+        there is basically left logic which from the formula is price - a + k
+        and right with is price + a - k
+        the thing is, inside the seg we do know a, so we abuse this fact and compute both
+        only missing the k variable on both sides, which does not affect the result since
+        no matter the k it will be constant for both so for the left we just need to find
+        the min(price - a)+k and from the right the min(price + a)-k
 */
 
 const ll maxn = 2e5+2;
 const ll inf = LONG_LONG_MAX/4;
 namespace seg {
     struct S { 
-        ll dist, pPrice, sPrice;
-        S(ll a = inf)  : dist(1), pPrice(a), sPrice(a) {} 
+        ll lPrice, rPrice;
+        S(ll a = inf, ll index = 0)  : lPrice(a-index), rPrice(a+index) {} 
     };
 
     S t[maxn*2];
@@ -27,11 +32,8 @@ namespace seg {
 
     S join (const S &a, const S &b) {
         S res;
-        res.dist = a.dist + b.dist;
-        res.sPrice = min(a.sPrice, b.sPrice + b.dist);
-        res.pPrice = min(b.pPrice, a.pPrice + a.dist);
-        printf("join(S{%d, %d, %d}, S{%d, %d, %d}} = ", a.dist, a.sPrice, a.pPrice, b.dist, b.sPrice, b.pPrice);
-        printf("res(S{%d, %d, %d}) \n", res.dist, res.sPrice, res.pPrice);
+        res.lPrice = min(a.lPrice, b.lPrice);
+        res.rPrice = min(a.rPrice, b.rPrice);
         return res;
     }
 
@@ -40,19 +42,15 @@ namespace seg {
     }
 
     void update(ll p, ll v) {
-        for (t[p+=n]=S(v); p>>=1;) t[p] = join(t[p<<1], t[p<<1|1]);
+        for (t[p+=n]=S(v, p+1); p>>=1;) t[p] = join(t[p<<1], t[p<<1|1]);
     }
 
     S query(ll l, ll r) {
         S resr, resl;
-        // resr.dist = resl.dist = 0;
         for (l+=n, r+=n; l < r; l>>=1, r>>=1) {
             if (r&1) resr = join(t[--r], resr);
             if (l&1) resl = join(resl, t[l++]);
-            // cout << "resr  r = " << r << ", p = " << resr.pPrice << ", s = " << resr.sPrice << endl;
-            // cout << "resl  l = " << l << ", p = " << resl.pPrice << ", s = " << resl.sPrice << endl << endl;
         }
-        // resr.dist = resl.dist = 0;
         return join(resl, resr);
     }
 }
@@ -61,11 +59,9 @@ int main(void) {
     ll n, q; cin >> n >> q;
     for (int i = 0; i < n; i++) {
         ll v; cin >> v;
-        seg::t[i+n] = seg::S(v);
+        seg::t[i+n] = seg::S(v, i+1);
     }
     seg::build(n);
-
-    for (int i = 0; i < n*2; i++) cout << seg::t[i].pPrice << " "; cout << endl;
 
     for (int i = 0; i < q; i++) {
         int op; cin >> op;
@@ -73,17 +69,12 @@ int main(void) {
             int k; cin >> k;
             auto l = seg::query(0, k);
             auto r = seg::query(k-1, n);
-            // cout << "l p = " << l.pPrice << endl;
-            // cout << "l s = " << l.sPrice << endl;
-            // cout << "r p = " << r.pPrice << endl;
-            // cout << "r s = " << r.sPrice << endl;
+            cout << min(l.lPrice+k, r.rPrice-k) << endl;
 
-            cout << min(l.pPrice, r.sPrice) << endl;
         } else {
-            // int k, x; cin >> k >> x;
-            // seg::update(k-1, x);
+            int k, x; cin >> k >> x;
+            seg::update(k-1, x);
         }
-
     }
     return 0;
 }
