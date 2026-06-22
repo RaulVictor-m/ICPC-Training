8dc73bbfaea3b14adb9daf553d46b02dc222eff6
RaulVictor-m <raulvictor822@gmail.com>
2026-06-22 09:32:00 -0300

    C, MPP

---
+++ paulista2026/C/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * LINK: https://codeforces.com/gym/106598/problem/C
+ * NAME: C
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+/*
+    REASON:
+*/
+
+int main(void) {
+    long n, m; cin >> n >> m;
+    cout << n*8/m << endl;
+    return 0;
+}
