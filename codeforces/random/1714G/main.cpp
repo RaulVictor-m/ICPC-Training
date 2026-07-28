0243adaf77e64d0cbd1119a956c49f932842413b
RaulVictor-m <raulvictor822@gmail.com>
2026-07-28 10:47:55 -0300

    1714G

--- codeforces/random/1714G/main.cpp
+++ codeforces/random/1714G/main.cpp
@@ -11,40 +11,38 @@ typedef long long ll;
 // just keep a dinamic prefix sum on the path to the node
 // and pop as i leave the node
 
-
 vector<vector<array<ll, 3>>> adj;
-vector<ll> prefa, prefb;
+vector<ll> pref;
 vector<ll> ans;
 
-void dfs(int v) {
-    if (v != 1) {
-        auto len = upper_bound(prefb.begin(), prefb.end(), prefa.back()) - prefb.begin();
-        ans[v] = len-1;
-    }
+void dfs(int v, ll acc) {
+    ans[v] = upper_bound(pref.begin(), pref.end(), acc) - pref.begin() -1;
 
     for (auto [u, a, b]: adj[v]) {
-        prefa.push_back(a+prefa.back());
-        prefb.push_back(b+prefb.back());
-        dfs(u);
-        prefa.pop_back();
-        prefb.pop_back();
+        pref.push_back(b+pref.back());
+        dfs(u, acc+a);
+        pref.pop_back();
     }
 }
 
 int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t; cin >> t;
+    pref.resize(1);
+
     while (t--) {
         int n; cin >> n;
         adj.clear(), adj.resize(n+1);
         ans.clear(), ans.resize(n+1);
-        prefa.resize(1), prefb.resize(1);
 
         for (int i = 2; i <= n; i++) {
             ll a, b, c; cin >> a >> b >> c;
             adj[a].push_back({i, b, c});
         }
 
-        dfs(1);
+        dfs(1, 0);
 
         for (int i = 2; i <= n; i++) cout << ans[i] << " ";
 
