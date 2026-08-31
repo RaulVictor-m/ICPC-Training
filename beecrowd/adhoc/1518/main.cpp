c548634ada4082dc28249b16532bab9f4cb993b5
RaulVictor-m <raulvictor822@gmail.com>
2026-08-31 09:25:06 -0300

    beecrowd

---
+++ beecrowd/adhoc/1518/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * LINK: https://judge.beecrowd.com/pt/problems/view/1518
+ * NAME: Tartarugas
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+    return 0;
+}
