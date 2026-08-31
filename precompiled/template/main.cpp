6b41a414ca4ed7e5c2d1e1fef471d396fa999a12
RaulVictor-m <raulvictor822@gmail.com>
2026-08-31 10:53:01 -0300

    template update

--- precompiled/template/main.cpp
+++ precompiled/template/main.cpp
@@ -6,13 +6,14 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+namespace rg = std::ranges;
+namespace rv = std::views;
+
 typedef long long ll;
 
 /*
     REASON:
-
 */
-
 
 int main(void) {
     return 0;
