3ede898d8d32249ee63eaf83009426ac934d206b
RaulVictor-m <raulvictor822@gmail.com>
2026-09-23 22:48:08 -0300

    cpp template fix

--- precompiled/template/main.cpp
+++ precompiled/template/main.cpp
@@ -2,11 +2,23 @@
  * LINK: 
  * NAME: 
 */
-#include <bits/stdc++.h>
 
+#include <bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
 
 int main(void) {
+    cin.tie(nullptr)->sync_with_stdio(false);
     return 0;
 }
+
